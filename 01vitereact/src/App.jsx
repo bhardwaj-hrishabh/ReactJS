497cbd5340b7015da827e04fbbf101bd2911b515
bhardwaj-hrishabh <bhardwajhrishabh07@gmail.com>
2024-02-20 20:33:03 +0530

    Basic React with Vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from "./chai"
+
+
+function App() {
+  const username = "Hrishabh Bhardwaj"
+  return (
+    <>
+      <h1> Welcome to Vite + React</h1>
+      <h2> {username} </h2>
+      <Chai/>
+    </>
+    
+  )
+}
+
+export default App
